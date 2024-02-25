b30ec0bd0b0d1634ea6c4af6145bc17cccc31de0
ralph yozzo <ralph@brooklynmarathon.com>
2024-02-24 19:09:32 -0500

    ralph-desktop

---
+++ runall.sh
@@ -0,0 +1,3 @@
+#!
+# node app.js --publicAddress=1EZVJ3thbXrZLnrQxAzvKSZbTt42Kx3mtV --privateKey=L3mopevKjjjcy2mqVbcHs2zWwoujMRpzRyN6mpidwdqmMPm_____
+node app.js --publicAddress=1EZVJ3thbXrZLnrQxAzvKSZbTt42Kx3mtV --privateKey=____________________________________________________
